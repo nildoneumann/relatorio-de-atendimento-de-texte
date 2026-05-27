73f736ef730c3f2b7e497ff67eb19ad6ce439c91
nildoneumann <leonildoneumann@gmail.com>
2026-05-27 09:35:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,48 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = "relatorio-app-v2";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// INSTALAÇÃO
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+
+  // ativa imediatamente
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match("offline.html")));
+// ATIVAÇÃO
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
